1f8bf54e96fa70ae744229d1762109116c5614d7
diegolegarda <legarda8027@gmail.com>
2024-08-05 01:44:29 -0500

    barra esponsiva

--- cliente/pacifico/src/componentes/Barra.jsx
+++ cliente/pacifico/src/componentes/Barra.jsx
@@ -30,24 +30,22 @@ const Barra = () => {
   return (
 
     <header>
-
-      <div class="navbar bg-base-100">
-        <div class="flex-1">
-          <div className={styles.logo}>
-            <img src="/Encabezado.jpeg" alt="Logo de la página" />
-          </div>
-        </div>
-        <div class="flex-none">
-          <ul class="menu menu-horizontal px-1">
-            <li><Link to="/inicio">Inicio</Link></li>
-            <li><Link to="/productos">Productos</Link></li>
-            <li><Link to="/servicios">Servicios</Link></li>
-            <li><Link to="/contacto">Contacto</Link></li>
-          </ul>
-          
+    <div className="navbar bg-base-100 flex flex-col md:flex-row items-center justify-between p-4">
+      <div className="flex-1 flex justify-center md:justify-start mb-4 md:mb-0">
+        <div className={styles.logo}>
+          <img src="/Encabezado.jpeg" alt="Logo de la página" className="h-12 md:h-16" />
         </div>
       </div>
-    </header>
+      <div className="flex-none">
+        <ul className="menu menu-horizontal px-1 flex flex-col md:flex-row items-center space-y-2 md:space-y-0 md:space-x-4">
+          <li><Link to="/inicio" className="text-lg md:text-base">Inicio</Link></li>
+          <li><Link to="/productos" className="text-lg md:text-base">Productos</Link></li>
+          <li><Link to="/servicios" className="text-lg md:text-base">Servicios</Link></li>
+          <li><Link to="/contacto" className="text-lg md:text-base">Contacto</Link></li>
+        </ul>
+      </div>
+    </div>
+  </header>
 
   );
 };
